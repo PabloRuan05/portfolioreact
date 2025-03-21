e334d9aff62c11a2f61ea9abea7242f0662f78f2
Pablo Muniz <apablor580@hotmail.com>
2025-03-21 11:13:24 -0300

    auali

--- src/components/CardProj/CardProj.jsx
+++ src/components/CardProj/CardProj.jsx
@@ -1,4 +1,6 @@
 import styles from "./styles.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { IoMdLink } from "react-icons/io";
 
 function CardProj(props) {
   return (
@@ -8,9 +10,18 @@ function CardProj(props) {
       </a>
       <h1>{props.title}</h1>
       <p className="description">{props.description}</p>
-      <a href={props.linkGit} target="blank">
-        <button>Conferir no Github</button>
-      </a>
+      <div className={styles.wrapper}>
+        <a className={styles.link} href={props.link} target="blank">
+          <i>
+            <IoMdLink />
+          </i>
+        </a>
+        <a className={styles.git} href={props.linkGit} target="blank">
+          <i>
+            <FaGithub />
+          </i>
+        </a>
+      </div>
     </div>
   );
 }
